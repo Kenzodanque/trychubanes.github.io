92e2ae9f271f624d3a05ac8b54b39e78399216c5
Kenzodanque <102001376+Kenzodanque@users.noreply.github.com>
2022-11-15 14:41:02 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php 
+
+include 'config.php';
+
+session_start();
+
+error_reporting(0);
+
+if (isset($_SESSION['username'])) {
+    header("Location: welcome.php");
+}
+
+if (isset($_POST['submit'])) {
+	$email = $_POST['email'];
+	$password = md5($_POST['password']);
+
+	$sql = "SELECT * FROM users WHERE email='$email' AND password='$password'";
+	$result = mysqli_query($conn, $sql);
+	if ($result->num_rows > 0) {
+		$row = mysqli_fetch_assoc($result);
+		$_SESSION['username'] = $row['username'];
+		header("Location: welcome.php");
+	} else {
+		echo "<script>alert('Woops! Email or Password is Wrong.')</script>";
+	}
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+	<title>Login</title>
+</head>
+<body>
+	<div class="container">
+		<form action="" method="POST" class="login-email">
+			<p class="login-text" style="font-size: 2rem; font-weight: 800;">Welcome</p>
+			<br>
+			
+			<div class="input-group">
+				<button class="btn"><a href="company.php">Company</a></button>
+			</div>
+			<br>
+			<br>
+
+			<div class="input-group">
+				<button class="btn"><a href="applicant.php">Applicant</a></button>
+			</div>
+
+
+		</form>
+	</div>
+</body>
+</html>
